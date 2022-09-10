4df09e19cf2beaa8a3a8c7fb15bb15c961aea044
Justin Chen <ch3njus@gmail.com>
2022-09-10 18:37:55 -0400

    Can scrape all metadata and pair institutions. Also added save scraped papers to temp folder before scraping metadata. And can reload starting from temp data.

--- api/icml.py
+++ api/icml.py
@@ -1,39 +1,81 @@
 '''
 '''
+import os
 import re
+import sys
+import json
 import urllib.request
 from urllib.error import URLError, HTTPError
+from collections import defaultdict
+from time import sleep, time
+from tqdm import tqdm
+from random import randint
 
+import api.utils as utils
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 class ICML(object):
 	def __init__(self, year):
 		self.year = str(year)
 		self.base = f'https://icml.cc/Conferences/' 
+  
+  
+	def capitalize_hyphenated(self, name):
+		return ('-'.join([i.capitalize() for i in name.split('-')])).title()
 
 
+	'''
+	'''
 	def get_institutions(self, eventID):
-		event_url = 'https://icml.cc/Conferences/2021/Schedule?showEvent={eventID}'
+		event_url = 'https://icml.cc/Conferences/2021/Schedule?showEvent='+str(eventID)
 		resp = urllib.request.urlopen(event_url)
-		soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='iso-8859-1')
-		tags = soup.find_all('button', {'class': 'btn btn-default'})
+		soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='utf-8')
+		tags = soup.find_all('button', {'class': 'btn'})
+		
 
-		print(soup)
+		def get_number(string):
+			return re.compile(r"\d+-\d+").findall(string)[0]
+
+		def remove_special_characters(string):
+			return ' '.join(s for s in string.strip().split() if len(re.sub('[^A-Za-z0-9]+', '', s)) > 0)
 
 		def get_institution(_id):
-			print(_id)
-			speaker_url = 'https://icml.cc/Conferences/2021/Schedule?showSpeaker={_id}'
-			resp = urllib.request.urlopen(speaker_url)
-			soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='iso-8859-1')
-			tags = soup.find_all('button', {'class': 'maincard Remark col-sm-12'})
+			speaker_url = 'https://icml.cc/Conferences/2021/Schedule?showSpeaker='+_id
+			try:
+				resp = urllib.request.urlopen(speaker_url)
+				soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='utf-8')
+				return soup.find_all('h4')[0].text
+			except Exception:
+				return ''
 
-			print(tags)
-			# return tags
+		institutions = defaultdict()
 
-		# src: https://stackoverflow.com/a/50122731/3158028
-		exp = '(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?'
-		institutions = [get_institution(re.search(exp, t.get('onclick'))) for t in tags]
-		
+		for t in tags:
+			tag =  t.get('onclick')
+			if tag and 'showSpeaker' in tag:
+				author = self.capitalize_hyphenated(remove_special_characters(t.text))
+				speaker_number = get_number(tag)
+				institutions[author] = get_institution(speaker_number)
+
+		return institutions
+
+
+	'''
+	'''
+	def combine_institutions(self, authors, labs, _id):
+		for i, auth in enumerate(authors):
+			auth_name = ' '.join([auth['given_name'], auth['family_name']])
+   
+			try:
+				authors[i]['institution'] = labs[auth_name]
+			except:
+				print('------\nError:\nid: ',_id, '\tauthor: ',auth_name)
+				pprint(authors[i])
+				pprint(labs)
+				print('\n------\n')
+    
+		return authors
 
 
 	'''
@@ -51,29 +93,65 @@ class ICML(object):
 		for a in authors.split('·'):
 			a = a.split()
 			res.append({
-				'given_name': ''.join(a[:-1]),
-				'family_name': a[-1] if len(a) > 1 else '',
+				'given_name': self.capitalize_hyphenated(' '.join(a[:-1]).capitalize()),
+				'family_name': self.capitalize_hyphenated(a[-1]).title() if len(a) > 1 else '',
 				'institution': None
 			})
 
 		return res
 
 
-	def accepted_papers(self):
-		classname = 'maincard narrower poster'
-		posters_url = ''.join([self.base, f'{self.year}/Schedule?type=Poster'])
-		resp = urllib.request.urlopen(posters_url)
-		soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='iso-8859-1')
-		tags = soup.find_all('div', {'class': classname})
+	def restart(self):
+		os.path.exists()
 
-		papers = []
 
-		for t in tags: 
-			tag_id = t.get('id').split('_')[-1]
-			title = t.find('div', {'class', 'maincardBody'}).text
-			authors = t.find('div', {'class', 'maincardFooter'}).text
-			papers.append({'id': tag_id, 'title': title, 'authors': self.format_auths(authors)})
+	'''
+	inputs:
+	use_checkpoint (bool) If True, use file in temp if it exists. Else start from scratch.
+ 
+	outputs:
+	papers (dict) Paper metadata
+ 	'''
+	def accepted_papers(self, use_checkpoint=True):
+		papers = {}
+  
+		if use_checkpoint:
+			files = os.listdir('./temp')
+			saved_temp_filename = ''
+			temp_file_prefix = f'icml{self.year}'
+			
+			for f in files:
+				if temp_file_prefix in f:
+					saved_temp_filename = f
+					break
+			
+			with open(os.path.join('temp', saved_temp_filename)) as saved_temp:
+				papers = json.load(saved_temp)
 
-		print(papers[0])
-		self.get_institutions(papers[0]['id'])
+		else:
+			classname = 'maincard narrower poster'
+			posters_url = ''.join([self.base, f'{self.year}/Schedule?type=Poster'])
+			resp = urllib.request.urlopen(posters_url)
+			soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='utf-8')
+			tags = soup.find_all('div', {'class': classname})
+
+			papers = []
+
+			for t in tqdm(tags): 
+				tag_id = t.get('id').split('_')[-1]
+				title = t.find('div', {'class', 'maincardBody'}).text
+				authors = t.find('div', {'class', 'maincardFooter'}).text
+				papers.append({'id': tag_id, 'title': title, 'authors': self.format_auths(authors)})
+	
+			utils.save_json('./temp', f'icml{self.year}-{time()}', papers)
+
+		# sys.exit(0)
+  
+		for i, paper in enumerate(tqdm(papers)):
+			labs = self.get_institutions(paper['id'])
+			papers[i]['authors'] = self.combine_institutions(paper['authors'], labs, paper['id'])
+   
+   
+		pprint(papers)
+		
 		return papers
--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 '''
 import os
 import configparser
+from time import time
 
 import api.metadata as md
 import api.utils as utils
 
 
 def main():
+	start_time = time()
 	config = configparser.ConfigParser(allow_no_value=True)
 	config.read('config.ini')
 	cfg = config['DEFAULT']
@@ -18,9 +20,14 @@ def main():
 
 	if not os.path.isdir(save_dir):
 		os.makedirs(save_dir, exist_ok=True) 
+  
+	if not os.path.isdir('temp'):
+		os.makedirs('temp', exist_ok=True)
 
 	metadata = md.conference(name, year).accepted_papers()
 	utils.save_json(save_dir, f'{name}_{year}', metadata)
+ 
+	print('total time: ', time() - start_time)
 	
 
 if __name__ == '__main__':
