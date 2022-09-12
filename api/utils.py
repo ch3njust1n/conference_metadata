f4ab0fc7ff04279daeb09bd823465d341129934d
Justin Chen <ch3njus@gmail.com>
2022-09-11 21:09:02 -0400

    Added metadata from CoRL and ACML. Also updated save_json to create dir if does not exist.

--- api/utils.py
+++ api/utils.py
@@ -13,6 +13,9 @@ filename (str)  Name of output file
 data     (list) JSON object
 '''
 def save_json(save_dir, filename, data):
+	
+	if not os.path.isdir(save_dir):
+		os.makedirs(save_dir, exist_ok=True)
 
 	with open(os.path.join(save_dir, filename+'.json'), 'w', encoding='utf-8') as file:
 		json.dump(data, file, ensure_ascii=False, indent=4)
